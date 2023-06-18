9f3bbb729287eb69e7f9282758d049359c7a81e4
KristianBarzilov06 <102013263+KristianBarzilov06@users.noreply.github.com>
2023-06-18 14:28:16 +0300

    Add files via upload

---
+++ FishingApi/FishingApi/Data/FishSpecies.cs
@@ -0,0 +1,14 @@
+﻿namespace FishingApi.Data
+{
+    public class FishSpecies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SizeRange { get; set; }
+        public string PreferredBait { get; set; }
+        public string FishingTechniques { get; set; }
+
+        //продължи си класовете и мисли как ще го красиш
+    }
+}
---
+++ FishingApi/FishingApi/Data/FishingDam.cs
@@ -0,0 +1,22 @@
+﻿namespace FishingApi.Data
+{
+    public class FishingDam
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string ContactNumber { get; set; }
+        public string WaterBodyType { get; set; }
+
+        public virtual ICollection<FishSpecies>? FishSpecies { get; set; }
+        public string Regulations { get; set; }
+        public string Amenities { get; set; }
+        public FishingDam()
+        {
+            FishSpecies = new HashSet<FishSpecies>();
+        }
+    }
+}
